4508cba0ef5bce92fdb94492cc1b256acef5faf2
Mohammad Hossein Ashoori <mh_ashoori@yahoo.com>
2022-02-05 16:57:07 +0330

    add some node.js functionality to project

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,18 +1,48 @@
+import { useRef } from "react";
+
 function HomePage() {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  const submitFormHandler = (event) => {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+
+    const reqBody = { email: enteredEmail, text: enteredFeedback };
+
+    fetch("/api/feedback", {
+      method: "POST",
+      body: JSON.stringify(reqBody),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  };
   return (
     <div>
       <h1>The Home Page</h1>
-      <form>
+      <form onSubmit={submitFormHandler}>
         <div>
           <label htmlFor="email">Your Email</label>
           <br />
-          <input type="email" id="email" />
+          <input type="email" id="email" ref={emailInputRef} />
         </div>
         <div>
           <label htmlFor="message">Your Feedback</label>
           <br />
-          <textarea name="feedback" id="feedback" cols="30" rows="10"></textarea>
+          <textarea
+            name="feedback"
+            id="feedback"
+            cols="30"
+            rows="10"
+            ref={feedbackInputRef}
+          ></textarea>
         </div>
+        <button>Send Feed back</button>
       </form>
     </div>
   );
